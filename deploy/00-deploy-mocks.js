2d620ee49ee035368c5eadc0f0da977b629f8717
AnoopVL <avlanjekar4@gmail.com>
2023-01-18 19:34:22 +0530

    +2-mocks_baseFee,gasFee;

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,6 +1,15 @@
 /*++++++++++++++++++++++++++++++++ 1 ++++++++++++++++++++++++++++++++*/
 const { deploymentChains } = require("../helpers-hardhat-config");
 
+/*++++++++++++++++++++++++++++++++ 2 ++++++++++++++++++++++++++++++++*/
+//this is the premium(gas fee) needed to enter
+const BASE_FEE = ethers.utils.parseEther("0.25");
+//Eth price is somewhat $1500
+//Chainlink nodes pay the gas fees to give us randomness and do external execution
+//SO they price of requests change based on the price of gas
+const GAS_PRICE_LINK = 1e9; //link per gas. calulate value based on the gas price of the chain.
+const args = [BASE_FEE, GAS_PRICE_LINK];
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -9,5 +18,14 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   if (deploymentChains.includes(network.name)) {
     log("Local network detected !! Deploying mocks...");
     //now we have to deploy a mock vrfcoordinator...
+    await deploy("VRFCoordinatorV2Mock", {
+      from: deployer,
+      log: true,
+      args: args,
+    });
+    log("Mocks is deployed !!");
+    log("-------------------------------------------------------------------");
   }
 };
+
+module.exports.tags = ["all", "mocks"];
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,9 +1,20 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const { developmentChains } = require("../helpers-hardhat-config");
 
 /*++++++++++++++++++++++++++++++++ 1 ++++++++++++++++++++++++++++++++*/
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  /*++++++++++++++++++++++++++++++++ 2 ++++++++++++++++++++++++++++++++*/
+  let vrfCoordinatorV2Address;
+
+  /*++++++++++++++++++++++++++++++++ 2 ++++++++++++++++++++++++++++++++*/
+  if (developmentChains.includes(network.name)) {
+    const VRFCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+  }
 
   const raffle = await deploy("Raffle", {
     from: deployer,
