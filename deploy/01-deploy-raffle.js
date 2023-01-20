1082dda11e523fb172a7509e0bbbd58b869f87e4
AnoopVL <avlanjekar4@gmail.com>
2023-01-21 00:31:25 +0530

    +3_helpers,deploy-raffle

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -9,17 +9,18 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   /*++++++++++++++++++++++++++++++++ 2 ++++++++++++++++++++++++++++++++*/
-  const chainID = network.config.chainId;
   let vrfCoordinatorV2Address;
-
+  /*++++++++++++++++++++++++++++++++ 3 ++++++++++++++++++++++++++++++++*/
+  const chainID = network.config.chainId;
   /*++++++++++++++++++++++++++++++++ 2 ++++++++++++++++++++++++++++++++*/
+
   if (developmentChains.includes(network.name)) {
     const VRFCoordinatorV2Mock = await ethers.getContract(
       "VRFCoordinatorV2Mock"
     );
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
   } else {
-    vrfCoordinatorV2Address = networkConfig[chainID];
+    vrfCoordinatorV2Address = networkConfig[chainID]["vrfCoordinatorV2"];
   }
 
   const raffle = await deploy("Raffle", {
--- helpers-hardhat-config.js
+++ helpers-hardhat-config.js
@@ -1,7 +1,11 @@
+const { ethers } = require("hardhat");
+
 const networkConfig = {
   5: {
     name: "goerli",
     vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    /*++++++++++++++++++++++++++++++++ 3 ++++++++++++++++++++++++++++++++*/
+    entranceFee: ethers.utils.parseEther("0.01"),
   },
 };
 const developmentChains = ["hardhat", "localhost"];
