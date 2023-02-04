412d84b27e124fb719f98e237016b1db0a42f21a
AnoopVL <avlanjekar4@gmail.com>
2023-02-04 21:38:54 +0530

    continued

--- test/units/Raffle.test.js
+++ test/units/Raffle.test.js
@@ -10,7 +10,7 @@ const {
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("Raffle Unit Tests", async function () {
+  : describe("Raffle Unit Tests", function () {
       let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval;
       const chainId = networkConfig.chainId;
       beforeEach(async function () {
@@ -24,7 +24,7 @@ const {
         raffleEntranceFee = await raffle.getEntranceFee();
         interval = await raffle.getInterval();
       });
-      describe("constructor", async function () {
+      describe("constructor", function () {
         it("Initailizes the raffle correctly", async function () {
           // Usually in our tests we have just 1 assert per "it" function
           const raffleState = await raffle.getRaffleState();
@@ -34,7 +34,7 @@ const {
           assert.equal(interval.toString(), networkConfig[chianId]["interval"]);
         });
       });
-      describe("enterRaffle", async function () {
+      describe("enterRaffle", function () {
         it("reverts when you don't pay enough", async function () {
           await expect(raffle.enterRaffle()).to.be.revertedWith(
             "Raffle_NotEnoughETHEntered"
@@ -63,7 +63,7 @@ const {
         });
         ///continued
       });
-      describe("checkUpKeep", async function () {
+      describe("checkUpKeep", function () {
         it("returns false if people haven't sent any ETH", async function () {
           await network.provider.send("evm_increaseTime", [
             interval.toNumber() + 1,
