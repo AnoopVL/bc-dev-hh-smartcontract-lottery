b3b8e80e717ea6dc05e08f6fcd1148dd8d0ee76f
AnoopVL <avlanjekar4@gmail.com>
2023-03-17 11:55:53 +0530

    sepolia updates- accounts updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
-      goerli: ETHERSCAN_API_KEY,
+      sepolia: ETHERSCAN_API_KEY,
     },
   },
   gasReporter: {
--- helpers-hardhat-config.js
+++ helpers-hardhat-config.js
@@ -13,7 +13,7 @@ const networkConfig = {
     entranceFee: ethers.utils.parseEther("0.01"),
     gasLane:
       "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-    subscriptionId: "9917",
+    subscriptionId: "518",
     callbackGasLimit: "500000",
     //callbackGasLimit: "50000",
     interval: "30",
