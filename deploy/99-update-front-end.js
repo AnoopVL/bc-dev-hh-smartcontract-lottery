c0367d38f7eadc2da1aca3ae26c636d80b874a86
AnoopVL <avlanjekar4@gmail.com>
2023-03-20 23:26:44 +0530

    errors removed

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -10,21 +10,36 @@ module.exports = async function () {
   if (process.env.UPDATE_FRONT_END) {
     console.log("Updating the front-end !!");
     updateContractAddresses();
+    updateAbi();
   }
 };
 
 async function updateContractAddresses() {
   const raffle = await ethers.getContract("Raffle");
   const chainId = network.config.chainId.toString();
-  const curretAddresses = JSON.parse(
+  const currentAddresses = JSON.parse(
     fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8")
   );
-  if (chainId in curretAddresses) {
-    if (!curretAddresses[chainId].include(raffle.address)) {
-      curretAddresses[chainId].push(raffle.address);
-    }
-    {
-      curretAddresses[chainId] = [raffle.address];
+  if (chainId in currentAddresses) {
+    if (!currentAddresses[chainId].includes(raffle.address)) {
+      currentAddresses[chainId].push(raffle.address);
     }
   }
+  {
+    currentAddresses[chainId] = [raffle.address];
+  }
+  fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses));
 }
+
+async function updateAbi() {
+  const raffle = await ethers.getContract("Raffle");
+  fs.writeFileSync(
+    FRONT_END_ABI_FILE,
+    raffle.interface.format(ethers.utils.FormatTypes.json)
+    /** if we use JSON instead of json in the above line, we getthe following error
+     * TypeError: The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView. Received an instance of Array
+     */
+  );
+}
+
+module.exports.tags = ["all", "frontend"];
